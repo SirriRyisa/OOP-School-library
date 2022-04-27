11e4e3a35bdd0055c1de7d3ac96cae59cb0d7c3c
SirriRyisa <sirriryisa@gmail.com>
2022-04-27 12:26:12 +0100

    Updated method as per reviewer

--- class/person.rb
+++ class/person.rb
@@ -1,19 +1,18 @@
 class Person
-  def initialize(id, age, name = "unknown", parent_permission = true)
+  def initialize(_id, age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = age
     @age = name
     @parent_permission = parent_permission
-end
-attr_reader id
-attr_reader :age
-attr_accessor :name
-attr_accessor :parent_permission
+  end
+  attr_accessor :id, :age, :name, :parent_permission
 end
 
-person_1 = Person.new(20, "Ryisa", true)
-puts person_1.id
-puts person_1.age
-puts person_1.name
-puts person_1.parent_permission
+def can_use_service?
+  @age >= 18 || @parent_permission
+end
+
+def of_age?
+  @age >= 18
+end
   
