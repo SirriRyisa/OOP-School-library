da8b0ceb24bcb8f75b7fe2dad4854297c1c47bbc
SirriRyisa <sirriryisa@gmail.com>
2022-04-27 11:39:01 +0100

    Fixed linters

--- class/person.rb
+++ class/person.rb
@@ -4,21 +4,15 @@ class Person
     @name = age
     @age = name
     @parent_permission = parent_permission
-end
-attr_accessor :id, :age, :name, :parent_permission
-end
+  end
+    attr_accessor :id,  :age,  :name,  :parent_permission
+  end
 
 def can_use_service?
   @age >= 18 || @parent_permission
 end
 
-def is_of_age?
+def of_age?
   @age >= 18
 end
-
-# person_1 = Person.new(20, 'Ryisa', true)
-# puts person_1.id
-# puts person_1.age
-# puts person_1.name
-# puts person_1.parent_permission
   
