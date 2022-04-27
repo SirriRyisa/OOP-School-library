fa1875d54e80fbf9921d22c52163f6d6020fdf3e
SirriRyisa <sirriryisa@gmail.com>
2022-04-27 11:15:38 +0100

    Fixed linters

--- class/person.rb
+++ class/person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(id, age, name = "unknown", parent_permission = true)
+  def initialize(id, age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = age
     @age = name
@@ -16,9 +16,9 @@ def is_of_age?
   @age >= 18
 end
 
-person_1 = Person.new(20, "Ryisa", true)
-puts person_1.id
-puts person_1.age
-puts person_1.name
-puts person_1.parent_permission
+# person_1 = Person.new(20, 'Ryisa', true)
+# puts person_1.id
+# puts person_1.age
+# puts person_1.name
+# puts person_1.parent_permission
   
--- class/student.rb
+++ class/student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom, :parent_permission
 
-  def initialize(age, name = "unknown", parent_permission = true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = nil
   end
